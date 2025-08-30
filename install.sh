ad583ebeee17b5a8b22625a8c5a9a89cc565357b
Ari <toro000000000000000@gmail.com>
2025-08-30 09:54:58 +0200

    refined installation script

--- install.sh
+++ install.sh
@@ -4,10 +4,8 @@ set -e
 SOURCE_FILE="dotmenu"
 INSTALL_DIR="/usr/local/bin"
 INSTALL_PATH="$INSTALL_DIR/$SOURCE_FILE"
-
-if [ "$EUID" -ne 0 ]; then
-  exec sudo "$0" "$@"
-fi
+DOTFILES_DIR="$HOME/dotfiles"
+SOURCE_DOTFILES="dotfiles"
 
 echo "Starting installation of '$SOURCE_FILE'..."
 
@@ -17,6 +15,32 @@ if [ ! -f $SOURCE_FILE ]; then
     exit 1
 fi
 
+if [ ! -d "$SOURCE_DOTFILES "]; then
+  echo "Skipping..."
+else
+  if [ ! -e "$DOTFILES_DIR" ]; then
+  cp -r "dotfiles" "$HOME/"
+  else 
+    echo "Found existing config directory."
+    response=$(printf "Yes\nNo" | fzf --prompt="Create a backup?" --height="20%")
+      case $response in 
+        "Yes")
+            mv "$DOTFILES_DIR" "${DOTFILES_DIR}_backup"
+            ;;
+        "No")
+            rm -fr "$DOTFILES_DIR"
+            ;;
+        *)
+            echo "Aborted. Exiting..."
+            exit 1
+            ;;1
+     esac 
+    cp "dotfiles" "${DOTFILES_DIR}"
+  fi
+fi
+if [ "$EUID" -ne 0 ]; then
+  exec sudo "$0" "$@"
+fi
 cp "$SOURCE_FILE" "$INSTALL_PATH"
 echo "Copy made"
 chmod +x "$INSTALL_PATH"
